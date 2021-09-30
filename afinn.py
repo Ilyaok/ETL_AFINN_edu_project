41df303a75e4106c19c2c3009eda86f36e4600ff
Ilya <i.goldovskiy@itfbgroup.ru>
2021-09-30 17:56:29 +0300

    Пункты 3 и 5 выполнены с нормализованной БД.

--- afinn.py
+++ afinn.py
@@ -41,12 +41,11 @@ def sentiment_count(st, lex):
 
 
 for row in cur.execute('SELECT tweet_text FROM tweets_db_normalized'):
-    print(row[0])
     s = clear_str(row[0])
     tweet_sentiments[row[0]] = sentiment_count(s, lexicon)
 
 for row in tweet_sentiments:
-    cur.execute('UPDATE tweets_db'
+    cur.execute('UPDATE tweets_db_normalized '
                 'SET tweet_sentiment = (?) '
                 'WHERE tweet_text = (?)',
                 (tweet_sentiments[row], row))
--- analysis.sql
+++ analysis.sql
@@ -1,24 +1,59 @@
+-- Наиболее счастливые страны (по коду страны), локация и пользователь, общая таблица
+SELECT country_code Most_happy_country,
+       location Most_happy_location,
+       name Most_happy_user,
+       tweet_text Most_happy_tweet,
+       tweet_sentiment AFINN_sentiment
+  FROM tweets_db_normalized tbn
+       JOIN
+       locations l ON tbn.id = l.id
+ WHERE tweet_sentiment = (
+                             SELECT MAX(tweet_sentiment) 
+                               FROM tweets_db_normalized
+                         );
+
+
+-- Наименее счастливые страны (по коду страны), локация и пользователь, общая таблица
+SELECT country_code Least_happy_country,
+       location Least_happy_location,
+       name Least_happy_user,
+       tweet_text Least_happy_tweet,
+       tweet_sentiment AFINN_sentiment
+  FROM tweets_db_normalized tbn
+       JOIN
+       locations l ON tbn.id = l.id
+ WHERE tweet_sentiment = (
+                             SELECT MIN(tweet_sentiment) 
+                               FROM tweets_db_normalized
+                         );
+
+
 -- Наиболее счастливая локация
 SELECT location,
        tweet_sentiment
-  FROM tweets_db
+  FROM tweets_db_normalized tbn
+       JOIN
+       locations l ON tbn.id = l.id
  WHERE tweet_sentiment = (
                              SELECT MAX(tweet_sentiment) 
-                               FROM tweets_db
+                               FROM tweets_db_normalized
                          )
 AND 
        location IS NOT NULL
  ORDER BY location;
 
 
+
  
 -- Наименнее счастливая локация
 SELECT location,
        tweet_sentiment
-  FROM tweets_db
+  FROM tweets_db_normalized tbn
+       JOIN
+       locations l ON tbn.id = l.id
  WHERE tweet_sentiment = (
                              SELECT MIN(tweet_sentiment) 
-                               FROM tweets_db
+                               FROM tweets_db_normalized
                          )
 AND 
        location IS NOT NULL
@@ -30,26 +65,24 @@ AND
 SELECT name,
        tweet_text,
        tweet_sentiment
-  FROM tweets_db
+  FROM tweets_db_normalized
  WHERE tweet_sentiment = (
                              SELECT MAX(tweet_sentiment) 
-                               FROM tweets_db
+                               FROM tweets_db_normalized
                          )
 AND 
        name IS NOT NULL
  ORDER BY name;
 
 
-
- 
 -- Наименнее счастливые пользователи
 SELECT name,
        tweet_text,
        tweet_sentiment
-  FROM tweets_db
+  FROM tweets_db_normalized
  WHERE tweet_sentiment = (
                              SELECT MIN(tweet_sentiment) 
-                               FROM tweets_db
+                               FROM tweets_db_normalized
                          )
 AND 
        name IS NOT NULL
