a46511ecef2072b4fda842d407e262e51d17c44a
Ilya <i.goldovskiy@itfbgroup.ru>
2021-10-05 22:33:57 +0300

    Оптимизирован указанный заказчиком участок кода.

--- load.py
+++ load.py
@@ -17,80 +17,58 @@ class DBLoad:
         self.data = []
         self.json_dir = json_dir
         self.db_dir = db_dir
+        self.tweet_structure = ['name', 'tweet_text', 'country_code', 'display_url', 'lang', 'created_at', 'location', 'tweet_sentiment']
 
-    def json_to_db(self):
+    def json_read(self):
         with open(self.json_dir) as f:
             for line in f:
                 self.data.append(json.loads(line))
 
+    def json_to_db(self):
         con = sqlite3.connect(self.db_dir)
         cur = con.cursor()
 
         # Создание строки из json-файла для загрузки в БД
         for line in self.data:
-            if 'delete' not in line.keys():
-                line_to_db = []
+            if 'delete' in line.keys():
+                continue
 
-                if line['user']:
-                    if line['user']['name']:
-                        line_to_db.append(line['user']['name'])
-                    else:
-                        line_to_db.append(None)
-                else:
-                    line_to_db.append(None)
+            line_to_db = [None] * len(self.tweet_structure)
 
-                if line['text']:
-                    line_to_db.append(line['text'])
-                else:
-                    line_to_db.append(None)
+            if line['user']:
+                if line['user']['name']:
+                    line_to_db[0] = line['user']['name']
 
-                if line['place']:
-                    if line['place']['country_code']:
-                        line_to_db.append(line['place']['country_code'])
-                    else:
-                        line_to_db.append(None)
-                else:
-                    line_to_db.append(None)
+            if line['text']:
+                line_to_db[1] = line['text']
 
-                if line['entities']:
-                    if line['entities']['urls']:
-                        if line['entities']['urls'][0]:
-                            if line['entities']['urls'][0]['display_url']:
-                                line_to_db.append(line['entities']['urls'][0]['display_url'])
-                            else:
-                                line_to_db.append(None)
-                        else:
-                            line_to_db.append(None)
-                    else:
-                        line_to_db.append(None)
-                else:
-                    line_to_db.append(None)
+            if line['place']:
+                if line['place']['country_code']:
+                    line_to_db[2] = line['place']['country_code']
 
-                if line['lang']:
-                    line_to_db.append(line['lang'])
-                else:
-                    line_to_db.append(None)
+            if line['entities']:
+                if line['entities']['urls']:
+                    if line['entities']['urls'][0]:
+                        if line['entities']['urls'][0]['display_url']:
+                            line_to_db[3] = line['entities']['urls'][0]['display_url']
 
-                if line['created_at']:
-                    line_to_db.append(line['created_at'])
-                else:
-                    line_to_db.append(None)
+            if line['lang']:
+                line_to_db[4] = line['lang']
 
-                if line['user']:
-                    if line['user']['location']:
-                        line_to_db.append(line['user']['location'])
-                    else:
-                        line_to_db.append(None)
-                else:
-                    line_to_db.append(None)
+            if line['created_at']:
+                line_to_db[5] = line['created_at']
 
-                line_to_db.append(0)
+            if line['user']:
+                if line['user']['location']:
+                    line_to_db[6] = line['user']['location']
 
-                # загрузка очередной строки данных в БД
-                cur.execute('INSERT INTO tweets_db '
-                            '(name, tweet_text, country_code, display_url, lang, created_at, location, tweet_sentiment)'
-                            'VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                            line_to_db)
+            line_to_db[7] = 0
+
+            # загрузка очередной строки данных в БД
+            cur.execute('INSERT INTO tweets_db '
+                        '(name, tweet_text, country_code, display_url, lang, created_at, location, tweet_sentiment)'
+                        'VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                        line_to_db)
 
         con.commit()
         con.close()
@@ -113,6 +91,7 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-# Создание экземляра класса Загрузки и загрузка данных в БД
+# Загрузка данных в БД
 dbl = DBLoad(args.json_dir, args.db_dir)
+dbl.json_read()
 dbl.json_to_db()
